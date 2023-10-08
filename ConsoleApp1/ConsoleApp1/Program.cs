92ab90685a36318f37cde757b3aef1f2da4785a1
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2023-10-08 20:10:08 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Запросить через констоль имя пользователся, затем поздороваться с ним.
+            //Изменить цыет фона и символов.
+            //Установить название в соответствии с именем пользователя.
+            //Отоброжать, включен ли CapsLock b NumLock
+            SetWindowSize(50, 50);
+            BackgroundColor = ConsoleColor.DarkBlue;
+            Clear();
+            ForegroundColor = ConsoleColor.White;
+            WriteLine("Как тебя зовут?");
+            string name = ReadLine();
+            WriteLine($"Привет, {name} !");
+            Title = name;
+            CursorVisible = false;
+            WriteLine("Caps LOck - " + (CapsLock ? "On": "Off"));
+            WriteLine("Namber LOck - " + (NumberLock ? "On" : "Off"));
+            ReadKey();
+
+
+
+
+
+        }
+    }
+}
